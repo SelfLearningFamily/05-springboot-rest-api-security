1c3873290f6fb262f7c837d0b017a8bc7da9a5e3
msiddiqui <msiddiqui@i2cinc.com>
2023-12-26 21:03:02 +0500

    05-springboot-rest-api-security:user and roles using JDBC
    Earlier we were using the in-memory users and roles. It was more like hard coding approach. And I informed you guys that I ll move this to the JDBC in the later commits.  The purpose of using those in-memory users was keeping the things simple to get a good understanding of authentication and authorization.
    
    Now that we have made ourselves familiar with the above mentioned concepts, the next thing is to use those users and roles from the databases. To achieve this, there are two possible ways. One Springboot provides us with out of the box support for user and roles. In which we don't have to right any additional code to manipulate data in database. But there is a constraint in this method and i.e. you have to use the same schema that is defined by the Springboot. I added a file for the required schema as per the springboot standard. It requires two tables i.e. users and authorities. Here authorities represents the user roles.
    
    And the second approach is to use  the custom database tables . But in that case, we'll have to write some additional JDBC related code changes.
    
    But for this commit, I am using the first approach. i.e. using spring defined schema for user and roles. Hence the very minimal code changes have done.

--- src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -5,15 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
+    UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(hanlder ->
+                hanlder.requestMatchers(HttpMethod.GET,"api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"api/employees/**").hasRole("ADMIN")
+                );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.csrf(csrf -> csrf.disable());
+       return httpSecurity.build();
+    }
+
+    /*
+       @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails jhon = User.builder()
                 .username("jhon")
@@ -37,17 +58,5 @@ public class SecurityConfig {
 
 
     }
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.authorizeHttpRequests(hanlder ->
-                hanlder.requestMatchers(HttpMethod.GET,"api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET,"api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST,"api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT,"api/employees/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE,"api/employees/**").hasRole("ADMIN")
-                );
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        httpSecurity.csrf(csrf -> csrf.disable());
-       return httpSecurity.build();
-    }
+    * */
 }
